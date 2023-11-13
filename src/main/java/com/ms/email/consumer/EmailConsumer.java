26e9170269e08fe5859999223769b25f381bfae6
Guilherme Fragnani Bez Fontana <guilhermefragnani@icloud.com>
2023-11-13 09:44:32 -0300

    finish

--- src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,14 +1,25 @@
 package com.ms.email.consumer;
 
 import com.ms.email.DTOs.EmailRecordDTO;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO){
-        System.out.println("Email sent to: " + emailRecordDTO.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDTO, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
